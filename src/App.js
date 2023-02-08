d49a06ece6aa8a8f5f9a477d608dc2b20d9da392
afeez01 <ardz.afeez@gmail.com>
2023-02-08 13:56:09 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 
 import './App.css';
 import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Hero from "./components/Hero";
 
 function App() {
   return (
-    <div className="App">
-     <Header/>
-    </div>
-  );
+    <Router>
+    <Switch>
+      <Route exact path="/">
+        <Header />
+        <Hero />
+      </Route>
+      
+    </Switch>
+  </Router>
+);
 }
 
 export default App;
